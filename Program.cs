066c656cbb6e621808e08f5654e4846018149b82
UmarADev <umargithub03@gmail.com>
2024-06-10 18:20:58 +0500

    Loop Add

--- Program.cs
+++ Program.cs
@@ -1,34 +1,42 @@
 ﻿using System;
 using System.Linq;
 
-PrintLine("Welcome to IELTS score Calculator");
+string userChoise = "y";
 
-Print("Speaking Score: ");
-decimal speakingScore = GetUserScore();
+while(userChoise == "y")
+{
+    PrintLine("Welcome to IELTS score Calculator");
+    
+    Print("Speaking Score: ");
+    decimal speakingScore = GetUserScore();
+    
+    Print("Listening Score: ");
+    decimal listeningScore = GetUserScore();
+    
+    Print("Writing Score: ");
+    decimal writingScore = GetUserScore();
+    
+    Print("Reading Score: ");
+    decimal readinScore = GetUserScore();
+    
+    decimal[] score = {
+        speakingScore,
+        listeningScore,
+        writingScore,
+        readinScore
+    };
+    
+    decimal avarageScore = score.Average();
+    int baseScore = (int)avarageScore;
+    decimal remainder = ExtractRemainder(avarageScore);
+    
+    avarageScore = baseScore + remainder;
+    string LevelName = IeltsLevelName(avarageScore);
+    PrintLine($"Your overall: {avarageScore}\nYour category: {LevelName}");
 
-Print("Listening Score: ");
-decimal listeningScore = GetUserScore();
-
-Print("Writing Score: ");
-decimal writingScore = GetUserScore();
-
-Print("Reading Score: ");
-decimal readinScore = GetUserScore();
-
-decimal[] score = {
-    speakingScore,
-    listeningScore,
-    writingScore,
-    readinScore
-};
-
-decimal avarageScore = score.Average();
-int baseScore = (int)avarageScore;
-decimal remainder = ExtractRemainder(avarageScore);
-
-avarageScore = baseScore + remainder;
-string LevelName = IeltsLevelName(avarageScore);
-PrintLine($"Your overall: {avarageScore}\nYour category: {LevelName}");
+    Console.Write("Do you want to run again?\nY / N :\n");
+    userChoise = Console.ReadLine().ToLower();
+}
 
 static void Print(string message)
 {
@@ -64,18 +72,16 @@ static decimal ExtractRemainder(decimal avarageScore)
         _ => 1,
     };
 }
-
 static decimal GetUserScore()
 {
     string userInput = Console.ReadLine();
-
     try
     {
         return Convert.ToDecimal(userInput);
     }
     catch (FormatException formatException)
     {
-        Console.WriteLine("The score you inserted was invalid.");
+        Console.WriteLine($"{formatException.Message}");
         return 0;
     }
 }
