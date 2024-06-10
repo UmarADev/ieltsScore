8c9cd3360261cef43e41b04fa88f4661a5204a0a
UmarADev <umargithub03@gmail.com>
2024-06-10 17:43:13 +0500

    Ielts Level Name Add

--- Program.cs
+++ Program.cs
@@ -9,3 +9,16 @@ static void PrintLine(string message)
 {
     Console.Write($"{message}");
 }
+
+static string IeltsLevelName(decimal score)
+{
+    return score switch
+    {
+        9 or 8.5m => "Expert",
+        8 or 7.5m => "Very Good",
+        7 or 6.5m => "Good",
+        6 or 5.5m => "Competent",
+        5 => "Modest",
+        _ => "Incompetent"
+    };
+}
